5cdd3f9fa7efca8cee97593db6486e515f93b816
genetyc <gigagrigiga@gmail.com>
2025-05-28 22:55:58 +0300

    main_fix

--- main.py
+++ main.py
@@ -11,21 +11,30 @@ async def set_commands():
 
 async def on_startup() -> None:
     # global file
-    await set_commands()
-    await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен!')
-    WEBHOOK_PATH = f"{WEBHOOK_URL}/{API}"
-    await bot.set_webhook(WEBHOOK_PATH)
-    # print("Bot is running...")
-    await db.connect()        
+    try:
+        await set_commands()
+        await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен!')
+        #await bot.set_webhook(WEBHOOK_PATH)
+        await bot.set_webhook(
+            url=f"{WEBHOOK_URL}/{API}",
+            drop_pending_updates=True
+        )
+        # print("Bot is running...")
+        await db.connect()
+    except Exception as e:
+        logging.error(f"Error while launching: {e}")
 
 
-from handlers import admin_panel, survey, mini_survey, start
 async def on_shutdown() -> None:
     await bot.send_message(chat_id=ADMIN_ID, text='Бот остановлен!')
+    await bot.delete_webhook()
     await db.close()
     await bot.session.close()
 
 
+from handlers import admin_panel, survey, mini_survey, start
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
 async def main() -> None:
     dp.include_router(start.start_router)
     dp.include_router(survey.survey_router)
@@ -33,16 +42,21 @@ async def main() -> None:
     dp.include_router(admin_panel.admin_router)
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
-    logging.basicConfig(level=logging.DEBUG)
-    #await dp.start_polling(bot)
-    await dp.start_webhook(
-        webhook_path=f'/{API}',
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host="0.0.0.0",
-        port=int(REMOTE_PORT)
-    )
+    
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot)
+    webhook_requests_handler.register(app, path=f"/{API}")
+    setup_application(app, dp, bot=bot)
+    
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host="0.0.0.0", port=int(REMOTE_PORT))
+    await site.start()
+    
+    while True:
+        await asyncio.sleep(3600)
 
 
 if __name__ == "__main__":
